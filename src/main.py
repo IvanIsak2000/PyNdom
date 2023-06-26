5794eda524a2c1a456f87bb7008ea317fa770dcc
IvanIsak2000 <vingving648@gmail.com>
2023-06-26 20:31:29 +0300

    added logo, added buttons

--- src/main.py
+++ src/main.py
@@ -1,22 +1,47 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Label, Button
+from textual.containers import Center
 
+import webbrowser
+import sys
+
+logo ='''
+
+8888888b.           888b    888      888                        
+888   Y88b          8888b   888      888                        
+888    888          88888b  888      888                        
+888   d88P 888  888 888Y88b 888  .d88888  .d88b.  88888b.d88b.  
+8888888P"  888  888 888 Y88b888 d88" 888 d88""88b 888 "888 "88b 
+888        888  888 888  Y88888 888  888 888  888 888  888  888 
+888        Y88b 888 888   Y8888 Y88b 888 Y88..88P 888  888  888 
+888         "Y88888 888    Y888  "Y88888  "Y88P"  888  888  888 
+                888                                             
+           Y8b d88P                                             
+            "Y88P"                                              
+
+'''
 
 class MainMenu(App[str]):
     def compose(self):
-        yield Label('PyRPG')
-        yield Button('Start', id='start')
-        yield Button('Settings', id='settings')
+        yield Center(Label(logo))
+        yield Center(Button('Start', id='start'))
+        yield Center(Button('Settings', id='settings'))
+        yield Center(Button('GitHub', id='github'))
+        yield Center(Button('Exit', id='exit'))
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
-        move = self.exit(event.button.id)
-        return move 
+        self.exit(str(event.button.id))
 
-
-class StartState(App[str]):
-    ...
 
 if __name__ == "__main__":
-    app = MainMenu()
-    reply = app.run()
-    print(reply)
+    user_choose_mode = MainMenu()
+    mode = user_choose_mode.run()
+    
+    if mode == 'start':
+        import start
+
+    elif mode == 'github':
+        webbrowser.open('https://github.com/IvanIsak2000/PyNdom')
+        
+    elif mode == 'exit':
+        sys.exit()
