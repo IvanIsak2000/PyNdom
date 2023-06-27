ac604bfb47027537257086ee5fb81714ce35c24b
IvanIsak2000 <vingving648@gmail.com>
2023-06-27 22:21:41 +0300

    test on ork class

--- src/start.py
+++ src/start.py
@@ -2,6 +2,22 @@ from textual.app import App, ComposeResult
 from textual.widgets import Input, Label, RadioButton, RadioSet, Button
 from textual.containers import Center, VerticalScroll
 
+ork = '''⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣧⣄⣉⣉⣠⣼⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⡿⣿⣿⣿⣿⢿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⣼⣤⣤⣈⠙⠳⢄⣉⣋⡡⠞⠋⣁⣤⣤⣧⠀⠀⠀⠀⠀⠀⠀
+        ⠀⢲⣶⣤⣄⡀⢀⣿⣄⠙⠿⣿⣦⣤⡿⢿⣤⣴⣿⠿⠋⣠⣿⠀⢀⣠⣤⣶⡖⠀
+        ⠀⠀⠙⣿⠛⠇⢸⣿⣿⡟⠀⡄⢉⠉⢀⡀⠉⡉⢠⠀⢻⣿⣿⡇⠸⠛⣿⠋⠀⠀
+        ⠀⠀⠀⠘⣷⠀⢸⡏⠻⣿⣤⣤⠂⣠⣿⣿⣄⠑⣤⣤⣿⠟⢹⡇⠀⣾⠃⠀⠀⠀
+        ⠀⠀⠀⠀⠘⠀⢸⣿⡀⢀⠙⠻⢦⣌⣉⣉⣡⡴⠟⠋⡀⢀⣿⡇⠀⠃⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⢸⣿⣧⠈⠛⠂⠀⠉⠛⠛⠉⠀⠐⠛⠁⣼⣿⡇⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠸⣏⠀⣤⡶⠖⠛⠋⠉⠉⠙⠛⠲⢶⣤⠀⣹⠇⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣶⣿⣿⣿⣿⣿⣿⣶⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠛⠛⠛⠛⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+'''
+
 class InputName(App):
     def compose(self) -> ComposeResult:
         yield Center(Label('Please write your name, stranger:'))
@@ -10,31 +26,49 @@ class InputName(App):
 
     def on_mount(self) -> None:
         self.screen.styles.background = "black"
-    
+
     def on_button_pressed(self, event: Button.Pressed) -> None:
-          self.exit(str(event.button.id))
+        self.exit(str(event.button.id))
+
 
 class CharacterSelection(App[None]):
+    CSS_PATH = "style.css"
+
     def compose(self) -> ComposeResult:
         with VerticalScroll():
-
             with RadioSet():
-                yield (RadioButton('Warrior'))
-                yield (RadioButton('Vampire'))
-                yield (RadioButton('Witch'))
-                yield (RadioButton('Elf '))
-                yield (RadioButton('Druid'))
-                yield (RadioButton('Rogue'))
+                yield RadioButton('Warrior')
+                yield RadioButton('Vampire')
+                yield RadioButton('Ork')
+                yield RadioButton('Witch')
+                yield RadioButton('Elf')
+                yield RadioButton('Druid')
+                yield RadioButton('Rogue')
 
     def on_radio_set_changed(self, event: RadioSet.Changed) -> None:
         self.exit(str(event.pressed.label))
-        
+
+
+class Ork(App[None]):
+
+    def compose(self) -> ComposeResult:
+        yield Label(ork)
+
+    def on_mount(self) -> None:
+        self.screen.styles.background = "black"
+
+
 username  = InputName()
 username = username.run()
 
 character = CharacterSelection()
 selection = character.run()
 print(selection)
+
+ork = Ork()
+ork = ork.run()
+
+
 
 
 
