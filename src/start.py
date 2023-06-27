faa5b0ea91aa3ac4079efee3def0b0bd09d688cc
IvanIsak2000 <vingving648@gmail.com>
2023-06-27 19:11:05 +0300

    added `CharacterSelection`

--- src/start.py
+++ src/start.py
@@ -1,24 +1,38 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Input, Label, Button
-from textual.containers import Center
+from textual.widgets import Input, Label, Button, Checkbox
+from textual.containers import Center, VerticalScroll
 
 class InputName(App):
     def compose(self) -> ComposeResult:
-
-        # first_message = []
-        # first_message +='Hey Ya! First? '
-        # first_message += "Before starting the game, "
-        # first_message +=  "let's decide on the settings!)
-
         yield Center(Label('Please write your name, stranger:'))
         yield Input(placeholder=" ", id='name')
-        yield Center(Button('Save!' ))
+        yield Center(Button('Save!', id='save'))
 
     def on_mount(self) -> None:
         self.screen.styles.background = "black"
+    
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+          self.exit(str(event.button.id))
 
-app = InputName()
-print(app.run())
+class CharacterSelection(App[None]):
+    def compose(self) -> ComposeResult:
+            yield Center(Label('Please choose character:'))
+            with VerticalScroll():
+                yield Center(Checkbox('Warrior'))
+                yield Center(Checkbox('Vampire'))
+                yield Center(Checkbox('Witch'))
+                yield Center(Checkbox('Elf '))
+                yield Center(Checkbox('Druid'))
+                yield Center(Checkbox('Rogue'))
+
+username  = InputName()
+username = username.run()
+
+character = CharacterSelection()
+print(character.run())
+
+
+
 
 
 
