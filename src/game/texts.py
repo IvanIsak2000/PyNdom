1e211f0334993fdb9cbe526da73746024769bd71
IvanIsak2000 <vingving648@gmail.com>
2023-06-28 18:33:57 +0300

    added characters logo, characters info

---
+++ src/game/texts.py
@@ -0,0 +1,15 @@
+warrior_health = '\nHealth: 7'
+warrior_power = 'Power: 6'
+warrior_starting_energy = 'Starting energy: 4'
+warrior_starting_weapon = 'Starting weapon: Gun\n'
+
+
+vampire_health = '\nHealth: 4'
+vampire_power = 'Power: 4'
+vampire_starting_energy = 'Starting energy: 7'
+vampire_starting_weapon = 'Starting weapon: Staff\n'
+
+ork_health = '\nHealth: 8'
+ork_power = 'Power: 3'
+ork_starting_energy = 'Starting energy: 8'
+ork_starting_weapon = 'Starting weapon: Baton\n'
